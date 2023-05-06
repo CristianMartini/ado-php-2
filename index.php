46e465247e59ab50b7956ddeb7db42f6f4b67d2b
Cristian martini <cristianmartini.sp@gmail.com>
2023-05-06 16:36:23 -0300

    SUBINDO NAVBAR

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Cadastro</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/Style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-sm fixed-top bg-primary-color" id="navbar">
+    <div class="container-sm ">
+      <a class="navbar-brand" href="index">
+        <img class="logo" src="imagem/Martini & Showza.png" alt="Logo" />
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-items"
+        aria-controls="navbar-items" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="bi bi-list"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar-items">
+        <ul class="navbar-nav me-auto  mb-lg-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="gerenciar">Gerenciar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="cadastro-produtos">Cadastro de Produtos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="estoque">Qualquer coisa</a>
+          </li>
+        </ul>
+      </div>
+      <div class="collapse navbar-collapse" id="logoff-btn">
+        <ul class="navbar-nav me-auto mb-lg-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link navbar-btn" aria-current="page" href="login">Logoff</a>
+          </li>
+        </ul>
+      </div>
+  </nav>
+</body>
+</html>
