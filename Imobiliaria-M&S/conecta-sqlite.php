4448adc51518662cd3b885e048675885c9c08790
Cristian martini <cristianmartini.sp@gmail.com>
2023-05-07 13:06:14 -0300

    criaçao dos arquivo e banco de dados

---
+++ Imobiliaria-M&S/conecta-sqlite.php
@@ -0,0 +1,11 @@
+<?php
+function conectar() {
+    try {
+        $db_file = "C:\\xampp\\htdocs\\Imobiliaria-M&S.sqlite";
+        return new PDO("sqlite:$db_file");
+    } catch (PDOException $e) {
+        echo "Erro: " . $e->getMessage();
+        throw $e;
+    }
+}
+?>
--- Imobiliaria-M&S/index.php
+++ Imobiliaria-M&S/index.php
@@ -5,16 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/Style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,700&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 <!-- NAVBAR -->
 <nav class="navbar navbar-expand-sm fixed-top bg-primary-color" id="navbar">
-    <div class="container-sm ">
+    <div class="container-md ">
       <a class="navbar-brand" href="index">
         <img class="logo" src="imagem/Martini & Showza.png" alt="Logo" />
       </a>
@@ -25,7 +26,7 @@
       <div class="collapse navbar-collapse" id="navbar-items">
         <ul class="navbar-nav me-auto  mb-lg-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="gerenciar">Gerenciar</a>
+            <a class="nav-link active" aria-current="page" href="gerenciar">Cadastro do Imovel</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="cadastro-produtos">Cadastro de Produtos</a>
@@ -43,5 +44,14 @@
         </ul>
       </div>
   </nav>
+
+
+
+
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
