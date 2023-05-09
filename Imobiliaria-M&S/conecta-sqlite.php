ee5904ed4016e683c26ae93a3118b9cbefdfbe5e
Cristian martini <cristianmartini.sp@gmail.com>
2023-05-08 22:09:36 -0300

    update conexao

--- Imobiliaria-M&S/conecta-sqlite.php
+++ Imobiliaria-M&S/conecta-sqlite.php
@@ -1,7 +1,7 @@
 <?php
 function conectar() {
     try {
-        $db_file = 'C:\xampp\htdocs\ado-php-2\Imobiliaria-M&S.sqlite';
+        $db_file = 'C:\xampp\htdocs\Imobiliaria-M&S\ado-php-2\Imobiliaria-M&S.sqlite';
         return new PDO("sqlite:$db_file");
     } catch (PDOException $e) {
         echo "Erro: " . $e->getMessage();
